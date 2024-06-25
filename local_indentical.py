54d6f59c126dada007b6d6ab36875f63a03a38c0
Yusuke Mukaiyama <mukaiyama.yusuke0424@gmail.com>
2024-06-25 13:18:48 +0900

    Initial commit or updated local changes

---
+++ local_indentical.py
@@ -0,0 +1,27 @@
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def local_authenticate():
+    creds = None
+    token_path = 'token.json'
+    creds_path = 'credentials.json'
+    
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(creds_path, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(token_path, 'w') as token:
+            token.write(creds.to_json())
+
+if __name__ == '__main__':
+    local_authenticate()
